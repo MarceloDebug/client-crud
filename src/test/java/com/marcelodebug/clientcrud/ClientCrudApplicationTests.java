28af2b605c39e0ce0c7b8295975e4c7d1e7e2a47
MarceloDebug <marcelinho.soares300@gmail.com>
2025-07-02 16:59:43 -0300

    first commit

---
+++ src/test/java/com/marcelodebug/clientcrud/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelodebug.clientcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
